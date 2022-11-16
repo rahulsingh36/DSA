b9b2487516ea0269cba52b12ec75f104525b5abc
rahulsingh36 <rahul1801.be21@chitkara.edu.in>
2022-11-16 07:17:00 +0530

    permutation_of_string

---
+++ permutation_of_string.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#include <bits/stdc++.h>
+
+class Solution {
+private:
+    void solve(vector<int> nums,vector<vector<int>>& ans , int index ){
+        if(index >= nums.size()){
+            ans.push_back(nums);
+            return ;
+        }
+        for(int i =index; i<nums.size();i++){
+            swap(nums[index], nums[i]);
+            solve(nums, ans, index+1);
+             // back track
+            swap(nums[index], nums[i]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index = 0;
+        solve(nums, ans, index);
+        return ans;
+    }
+};
+
+int main(){
+    vector<int> call ;
+    call.push_back(123);
+    permute(call);
+} 
