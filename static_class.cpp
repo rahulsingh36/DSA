abd80299a0a245036d97545838de0c1a2c92c4ae
rahulsingh36 <rahul1801.be21@chitkara.edu.in>
2022-11-17 14:49:43 +0530

    static class

---
+++ static_class.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+#include<string.h>
+
+class Rahul {
+private:
+    int phoneno;
+public:
+    char *name;
+    int rollno;
+
+    static int timetocomplete;
+
+    static int random(){
+        return timetocomplete;
+    }
+};
+
+// initilise of static
+int Rahul::timetocomplete = 5;
+
+int main(){
+    cout<<Rahul::timetocomplete<<endl;
+    Rahul a;
+    cout<<a.timetocomplete<<endl;
+
+    cout<< Rahul::random()<<endl;
+}
