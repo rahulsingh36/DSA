c3fc37657703d677b4276c8ccf6301433630296c
Rahul Singh <rahul1801.be21@chitkara.edu.in>
2022-11-18 08:19:22 +0530

    run time polymorphism

---
+++ run_time_polymorphism.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+#include<string.h>
+
+class A{
+    public:
+    void show(){
+        cout<<"Inside A"<<endl;
+    }
+};
+
+class B: public A{
+    public:
+    void show(){
+        cout<<"Inside B"<<endl;
+    }
+};
+class c: public A{
+    public:
+    void show(){
+        cout<<"Inside C"<<endl;
+    }
+};
+
+int main(){
+    A c1;
+    B c2;
+    c1.show();
+    c2.show();
+
+}
