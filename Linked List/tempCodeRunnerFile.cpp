66fbdc7f679804e05ea311198da4a478c261c4a8
Rahul Singh <rahul1801.be21@chitkara.edu.in>
2022-11-23 08:20:19 +0530

    temperory file

--- Linked List/tempCodeRunnerFile.cpp
+++ Linked List/tempCodeRunnerFile.cpp
@@ -36,48 +36,6 @@ void printhead(Node* &head){
     }
 }
 
-void removeduplicate(Node* &head){
-    Node* current = head;
-    Node* next_next;   
-    if(head == NULL){
-        return;
-    }
-    while(current->next != NULL){
-        if(current->data == current ->next->data){
-            next_next = current->next->next;
-            delete (current->next);
-            current->next = next_next;
-        }
-        else{
-            current = current->next;
-        }
-    }
-}
-void deleteddulpicateusingmap(Node* &head){
-    Node *ptr1, *ptr2, *dup;
-    ptr1 = head;
- 
-    /* Pick elements one by one */
-    while (ptr1 != NULL && ptr1->next != NULL) {
-        ptr2 = ptr1;
- 
-        /* Compare the picked element with rest
-           of the elements */
-        while (ptr2->next != NULL) {
-            /* If duplicate then delete it */
-            if (ptr1->data == ptr2->next->data) {
-                /* sequence of steps is important here */
-                dup = ptr2->next;
-                ptr2->next = ptr2->next->next;
-                delete (dup);
-            }
-            else /* This is tricky */
-                ptr2 = ptr2->next;
-        }
-        ptr1 = ptr1->next;
-    }
-}
-
 int main(){
     Node* head = NULL;
     insertAtHead(head,10);
@@ -86,9 +44,5 @@ int main(){
     insertAtHead(head,20);
     insertAtHead(head,25);
     insertAtHead(head,10);
-    printhead(head);
-    cout<<endl;
-    // removeduplicate(head);
-    deleteddulpicateusingmap(head);
     printhead(head);
 }
