a17a4fe3996071de3a21671a1cf04d36fe53bf77
rahulsingh36 <rahul1801.be21@chitkara.edu.in>
2022-12-13 14:52:53 +0530

    Linked List

--- Linked List/linked_list.cpp
+++ Linked List/linked_list.cpp
@@ -1,7 +1,33 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-#include<string.h>
 
-int main(){
+bool checkUniqueFrequency(int arr[],int n)
+{
+	unordered_map<int, int> freq;
+	for (int i = 0; i < n; i++) {
+		freq[arr[i]]++;
+	}
 
+	unordered_set<int> uniqueFreq;
+	for (auto& i : freq) {
+		if (uniqueFreq.count(i.second))
+			return false;
+		else
+			uniqueFreq.insert(i.second);
+	}
+	return true;
+}
+
+// Driver Code
+int main()
+{
+	int arr[] = { 1, 1, 2, 5, 5 };
+	int n = sizeof arr / sizeof arr[0];
+	bool res = checkUniqueFrequency(arr, n);
+
+	if (res)
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+	return 0;
 }
