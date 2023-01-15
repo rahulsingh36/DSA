325fac8183d78f8c682ce5800ae8a626e2f800a6
rahulsingh36 <rahul1801.be21@chitkara.edu.in>
2023-01-15 20:28:49 +0530

    Data types and variables

---
+++ Introduction/Data_type_and_size_of_variables.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = 1;
+    cout<< a <<endl;
+    cout<< "size of integer is " << sizeof(a)<<endl;
+
+    char c = 'a';
+    cout<< c <<endl;
+    cout<< "size of character is " << sizeof(c)<<endl;
+
+    bool b = true;
+    cout<< b <<endl;
+    cout<< "size of bool is " << sizeof(b)<<endl;
+
+    float f = 1.2;
+    cout<< f <<endl;
+    cout<< "size of float is " << sizeof(f)<<endl;
+
+    double d = 1.21;
+    cout<< d <<endl;
+    cout<< "size of double is " << sizeof(d)<<endl;
+
+}
