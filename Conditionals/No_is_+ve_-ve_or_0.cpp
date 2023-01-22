55f0a5d8027f111084d6218a986ff39e6f0bd289
rahulsingh36 <rahul1801.be21@chitkara.edu.in>
2023-01-22 10:39:35 +0530

    Number is positive negative and zero

---
+++ Conditionals/No_is_+ve_-ve_or_0.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter the number : ";
+    cin>>a;
+
+    if(a>0){
+        cout<<"The number is positive"<<endl;
+    }
+    else if(a<0){
+        cout<<"The number is negative"<<endl;
+    }
+    else{
+        cout<<"The number is zero"<<endl;
+    }
+}
