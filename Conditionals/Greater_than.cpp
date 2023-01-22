cdee8d8d7daca0f33a91bd053b3873541e25bfa9
rahulsingh36 <rahul1801.be21@chitkara.edu.in>
2023-01-22 10:41:21 +0530

    Greater than

---
+++ Conditionals/Greater_than.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter the value of A : ";
+    cin>>a;
+    int b = 10;
+    cout<<"Enter the value of B : ";
+    cin>>b;
+    if(a>b){
+        cout<<"A is greater"<<endl;
+    }
+    else{
+        cout<<"B is greater"<<endl;
+    }
+}
