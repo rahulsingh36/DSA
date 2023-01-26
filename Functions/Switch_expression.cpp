d235c1bb8d11a073672d8969c3de95567ee9ea0f
rahulsingh36 <rahul1801.be21@chitkara.edu.in>
2023-01-26 13:20:43 +0530

    Switch expression

---
+++ Functions/Switch_expression.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num = 2;
+    switch(num){
+        case 1:
+            cout<<"First"<<endl;
+            break;
+        case 2:
+            cout<<"Second"<<endl;
+            break;
+        default:
+            cout<<"This the deafault case"<<endl;
+    }
+    return 0;
+}
